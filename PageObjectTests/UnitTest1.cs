80ba106a89302a170081bb27b60f9551f051de3f
ladap1999 <ladap1999>
2023-06-05 16:10:39 +0300

    add new .gitignore file

---
+++ PageObjectTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace PageObjectTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
